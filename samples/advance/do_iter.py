aed8fae0c5fc10910cfb60bf888246cfa95a45ed
Michael Liao <askxuefeng@gmail.com>
2015-05-16 21:34:21 +0800

    update iter sample

--- samples/advance/do_iter.py
+++ samples/advance/do_iter.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import Iterable
-print('iterable? [1, 2, 3]:', isinstance([1, 2, 3], Iterable))
-print('iterable? \'abc\':', isinstance('abc', Iterable))
-print('iterable? 123:', isinstance(123, Iterable))
+from collections import Iterable, Iterator
+
+def g():
+    yield 1
+    yield 2
+    yield 3
+
+print('Iterable? [1, 2, 3]:', isinstance([1, 2, 3], Iterable))
+print('Iterable? \'abc\':', isinstance('abc', Iterable))
+print('Iterable? 123:', isinstance(123, Iterable))
+print('Iterable? g():', isinstance(g(), Iterable))
+
+print('Iterator? [1, 2, 3]:', isinstance([1, 2, 3], Iterator))
+print('Iterator? iter([1, 2, 3]):', isinstance(iter([1, 2, 3]), Iterator))
+print('Iterator? \'abc\':', isinstance('abc', Iterator))
+print('Iterator? 123:', isinstance(123, Iterator))
+print('Iterator? g():', isinstance(g(), Iterator))
 
 # iter list:
-print('iter [1, 2, 3, 4, 5]')
+print('for x in [1, 2, 3, 4, 5]:')
 for x in [1, 2, 3, 4, 5]:
     print(x)
+
+print('for x in iter([1, 2, 3, 4, 5]):')
+for x in iter([1, 2, 3, 4, 5]):
+    print(x)
+
+print('next():')
+it = iter([1, 2, 3, 4, 5])
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
 
 d = {'a': 1, 'b': 2, 'c': 3}
 
