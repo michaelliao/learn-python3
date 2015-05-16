9c9ef92398af67bff267921db4963b0243072f31
Michael Liao <askxuefeng@gmail.com>
2015-05-16 22:32:20 +0800

    update reduce sample

--- samples/functional/do_reduce.py
+++ samples/functional/do_reduce.py
@@ -23,3 +23,39 @@ def str2int(s):
 print(str2int('0'))
 print(str2int('12300'))
 print(str2int('0012345'))
+
+CHAR_TO_FLOAT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '.': -1
+}
+
+def str2float(s):
+    nums = map(lambda ch: CHAR_TO_FLOAT[ch], s)
+    point = -1
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 0
+            return f
+        if point == -1:
+            return f * 10 + n
+        else:
+            point = point + 1
+            return f + n / pow(10, point)
+    return reduce(to_float, nums, 0.0)
+
+print(str2float('0'))
+print(str2float('123.456'))
+print(str2float('123.45600'))
+print(str2float('0.1234'))
+print(str2float('.1234'))
+print(str2float('120.0034'))
