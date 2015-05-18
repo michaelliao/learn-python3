392e5291b064f0c0de57e7293a16262993ac1b2c
Michael Liao <askxuefeng@gmail.com>
2015-05-18 10:16:53 +0800

    add err raise, re-raise sample

---
+++ samples/debug/err_raise.py
@@ -0,0 +1,12 @@
+# err_raise.py
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
+
---
+++ samples/debug/err_reraise.py
@@ -0,0 +1,17 @@
+# err_reraise.py
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
+
