b3b303e68b6a1113e250e08e86a381d4094a553c
Michael Liao <askxuefeng@gmail.com>
2015-05-18 10:09:50 +0800

    add err_logging

---
+++ samples/debug/err_logging.py
@@ -0,0 +1,19 @@
+# err_logging.py
+
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
