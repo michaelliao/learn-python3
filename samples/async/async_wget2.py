ac5faaf650ad4288e2dd304384b2225111fde0a9
Michael Liao <askxuefeng@gmail.com>
2015-11-03 09:59:11 +0800

    add async/await sample

---
+++ samples/async/async_wget2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import threading
+import asyncio
+
+async def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    await asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
