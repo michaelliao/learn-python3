e72864fb66cbaf19c60fad94b4f10cb9c9b39668
Michael Liao <askxuefeng@gmail.com>
2017-04-24 09:08:55 +0800

    Update recur.py

--- samples/function/recur.py
+++ samples/function/recur.py
@@ -16,9 +16,9 @@ print('fact(10) =', fact(10))
 def move(n, a, b, c):
     if n == 1:
         print('move', a, '-->', c)
-        return
-    move(n-1, a, c, b)
-    print('move', a, '-->', c)
-    move(n-1, b, a, c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
 
 move(4, 'A', 'B', 'C')
