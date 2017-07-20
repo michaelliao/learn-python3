c13a150fad7ada3d72cd5bbf805aba100ced7f58
Zuoweixiang <Zuoweixiang@users.noreply.github.com>
2017-07-20 18:00:27 +0800

    移动时打印每个盘子的编号
    移动时打印每个盘子的编号

--- samples/function/recur.py
+++ samples/function/recur.py
@@ -22,3 +22,23 @@ def move(n, a, b, c):
         move(n-1, b, a, c)
 
 move(4, 'A', 'B', 'C')
+
+
+
+global index
+index = 0
+def __move(mark, from_,to):
+    global index
+    index =index + 1
+    print('步骤：',index,'盘子编号：',mark,from_,'->',to )
+
+def hannoi(n,start,end,middle):
+    if n==1 :
+        __move(n,start ,end )
+    else:
+        hannoi(n-1,start,middle,end )
+        __move(n,start,end )
+        hannoi(n-1,middle,end,start)
+
+
+hannoi(4,'start','end','middle')
