d4fc7c1fda5e1a65661bfc9ee379a90b9739c684
Frost Wong <frostwong@gmail.com>
2018-04-18 14:43:28 +0800

    Update task_worker.py
    fix typo

--- samples/multitask/task_worker.py
+++ samples/multitask/task_worker.py
@@ -30,7 +30,7 @@ for i in range(10):
         r = '%d * %d = %d' % (n, n, n*n)
         time.sleep(1)
         result.put(r)
-    except Queue.Empty:
+    except queue.Empty:
         print('task queue is empty.')
 # 处理结束:
 print('worker exit.')
