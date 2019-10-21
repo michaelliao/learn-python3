31f68211affc107b7fbf529373bac3f191fedabc
hubersimone <56387239+hubersimone@users.noreply.github.com>
2019-10-21 16:01:44 +0200

    cambio linguaggio da cinese a inglese

--- samples/basic/do_for.py
+++ samples/basic/do_for.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 打印list:
+# printlist:
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
     print(name)
 
-# 打印数字 0 - 9
+# printNumber 0 - 9
 for x in range(10):
     print(x)
