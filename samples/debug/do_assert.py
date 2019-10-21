5a6a4485af82122050abe69aa19f0bc172102b0b
Andrea Pollini <prof.andrea.pollini@gmail.com>
2019-10-21 16:02:41 +0200

    aggiunto esempio di controllo lunghezza stringa

--- samples/debug/do_assert.py
+++ samples/debug/do_assert.py
@@ -6,7 +6,13 @@ def foo(s):
     assert n != 0, 'n is zero!'
     return 10 / n
 
+def debug_msg(msg):
+    assert len(msg) > 0
+    print(msg)
+
 def main():
     foo('0')
+    debug_msg('debug')
+    debug_msg('')
 
 main()
