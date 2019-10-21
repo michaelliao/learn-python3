2575d24795cb23d976ba47d4e567871aea7ccad3
geaxx <56831186+geaxx@users.noreply.github.com>
2019-10-21 15:50:21 +0200

    traduzione-commenti-italiano

--- samples/basic/do_while.py
+++ samples/basic/do_while.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 计算1+2+3+...+100:
+# calcolare 1+2+3+...+100:
 sum = 0
 n = 1
 while n <= 100:
@@ -9,7 +9,7 @@ while n <= 100:
     n = n + 1
 print(sum)
 
-# 计算1x2x3x...x100:
+# calcolare 1x2x3x...x100:
 acc = 1
 n = 1
 while n <= 100:
