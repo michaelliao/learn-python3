12a7317c305e84fb27c56f5551d2d1f7cd6253e0
李典金 <30926725+lidianjin@users.noreply.github.com>
2019-10-30 12:03:53 +0800

    Update use_threadlocal.py
    初学Python者, 可能很容易混淆Thread的name, 和sutdent的name, 因此把student的name改为student_name, 这样代码更容易理解.

--- samples/multitask/use_threadlocal.py
+++ samples/multitask/use_threadlocal.py
@@ -3,21 +3,25 @@
 
 import threading
 
-# 创建全局ThreadLocal对象:
+
+# 创建全局ThreadLocal对象
 local_school = threading.local()
 
-def process_student():
-    # 获取当前线程关联的student:
-    std = local_school.student
-    print('Hello, %s (in %s)' % (std, threading.current_thread().name))
 
-def process_thread(name):
-    # 绑定ThreadLocal的student:
-    local_school.student = name
+def process_student():
+    """获取当前线程关联的student"""
+    student_name = local_school.student
+    print(f"Hello, {student_name}(in {threading.current_thread().name})")
+
+
+def process_thread(student_name):
+    """绑定ThreadLocal的student"""
+    local_school.student = student_name
     process_student()
 
-t1 = threading.Thread(target= process_thread, args=('Alice',), name='Thread-A')
-t2 = threading.Thread(target= process_thread, args=('Bob',), name='Thread-B')
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
 t1.start()
 t2.start()
 t1.join()
