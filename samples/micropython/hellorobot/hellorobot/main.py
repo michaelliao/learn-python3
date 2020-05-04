6141e7d0b1a1027caa27be5972a1f8765727aa5e
Michael Liao <askxuefeng@gmail.com>
2020-05-05 07:46:13 +0800

    add hellorobot

---
+++ samples/micropython/hellorobot/hellorobot/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+brick.sound.beep()
+# 输出到控制台:
+print('Hello, robot!')
+# 输出到EV3屏幕:
+brick.display.text('Hello, robot!')
