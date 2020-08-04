a2ce9f9d090abb5f2fe3bc52a96f0d373d0ed63a
Saumya Rai <42582943+SaumyaRai2010@users.noreply.github.com>
2020-08-04 19:13:29 +0530

    Updated_do_for.py
    Added the increment by any user value in the for loop by using the range()
    Example of pass keyword so that no error pops up if by chance your loop is empty.
    These are some important functions of the range and pass keywords.

--- samples/basic/do_for.py
+++ samples/basic/do_for.py
@@ -9,3 +9,9 @@ for name in names:
 # 打印数字 0 - 9
 for x in range(10):
     print(x)
+#Increment the sequence with 3 (default is 1):
+for x in range(2, 30, 3):
+  print(x)
+#if for some reason you have a for loop with no content, put in the pass statement to avoid getting an error
+for x in [0, 1, 2]:
+  pass
