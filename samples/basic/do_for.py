4a97aeaa1a976f99ee723b3e84f446bbc780d70c
VaishnaviShet <53848002+VaishnaviShet@users.noreply.github.com>
2020-10-01 16:35:34 +0530

    Updated do_for.py
    Added another for loop which shows the use of step parameter.

--- samples/basic/do_for.py
+++ samples/basic/do_for.py
@@ -9,3 +9,9 @@ for name in names:
 # 打印数字 0 - 9
 for x in range(10):
     print(x)
+    
+#prints even numbers from 0-10
+#here, 0-start, 11 - end, and 2 - step
+for y in range(0, 11, 2):
+    print(y)
+    
