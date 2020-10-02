b95e807e1f11e06e4f9afa980a46ccd44dd1b6e1
Lakhan840 <72196041+Lakhan840@users.noreply.github.com>
2020-10-02 17:46:44 +0530

    the_new_set.py
    I have added some new codes in this and also added some exercises also.
    I update it fully ok.

--- samples/basic/the_set.py
+++ samples/basic/the_set.py
@@ -1,8 +1,42 @@
+
+
+# Please read this below comment carefully
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-s1 = set([1, 1, 2, 2, 3, 3])
-print(s1)
-s2 = set([2, 3, 4])
-print(s1 & s2)
-print(s1 | s2)
+set1 = set([1, 1, 2, 2, 3, 3])
+print(set1)
+set2 = set([2, 3, 4])
+print(set1 & set2)
+print(set1 | set2)
+
+# Now you can see here that set are mutable
+
+myset = {"Whoops", "how", "ohh!"}
+
+myset.add("what")
+
+print(myset)
+
+# Exercise for you
+
+''' empties the following set
+
+'''
+
+A_set = {"apple", "banana", "cherry"}
+
+A_set.# (use this 'clear()' function)
+
+print(A_set)
+
+''' 
+# Answer of this problem
+A_set = {"apple", "banana", "cherry"}
+
+A_set.clear()
+
+print(A_set) '''
+
+
+
