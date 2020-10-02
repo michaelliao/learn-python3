1fa5c9c1cc4a586a0a6d5d3bd2e31b3248465b6b
anshu189 <65893784+anshu189@users.noreply.github.com>
2020-10-03 01:16:48 +0530

    Code added
    Code added- Updated the code and makes it more understandable to the user/programmer.

--- samples/function/call_func.py
+++ samples/function/call_func.py
@@ -3,7 +3,7 @@
 
 x = abs(100)
 y = abs(-20)
-print(x, y)
-print('max(1, 2, 3) =', max(1, 2, 3))
-print('min(1, 2, 3) =', min(1, 2, 3))
-print('sum([1, 2, 3]) =', sum([1, 2, 3]))
+print("Absolute value for 100 is:", x, "\nAbsolute value for -20 is:", y)
+print('Maximum number among (1, 2, 3) =', max(1, 2, 3))
+print('Minimum number among (1, 2, 3) =', min(1, 2, 3))
+print('Sum of (1, 2, 3) =', sum([1, 2, 3]))
