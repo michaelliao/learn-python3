b1b4bff7459550f740f5865cfc2d65d0e67c6c09
MZH-YES <74284914+MZH-YES@users.noreply.github.com>
2020-12-08 20:36:15 +0800

    Update do_slice.py

--- samples/advance/do_slice.py
+++ samples/advance/do_slice.py
@@ -14,3 +14,43 @@ print('R[-10:] =', R[-10:])
 print('R[10:20] =', R[10:20])
 print('R[:10:2] =', R[:10:2])
 print('R[::5] =', R[::5])
+
+
+#练习答案
+def trim(s):
+    a = None
+    b = None
+    c = 0
+    if s == '':
+        return ''
+    # elif s =='    ':
+    #     return ''
+    else:
+        for i in range(len(s)):
+            if s[i] == ' ':
+                c = c + 1
+                if c == len(s):
+                    return ''
+        for i in range(len(s)):
+            if a != None:
+                break
+            if s[i] == ' ':
+                continue
+            else:
+                a = i
+        for i in range(len(s)):
+            if b != None:
+                break
+            if s[-(i + 1)] == ' ':
+                continue
+            else:
+                b = len(s) - (i + 1) + 1
+            if a < b:
+                return (s[a:b])
+            # elif a == None:
+            #     return ''
+            else:
+                break
+            ++i
+
+    
