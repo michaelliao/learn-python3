fa8954e169cba8d505b2316165ba5c166f78c78e
Gotam Gorabh <55174501+GOTAM672@users.noreply.github.com>
2022-01-20 19:24:36 +0530

    adding key-value pair to dic

--- samples/basic/the_dict.py
+++ samples/basic/the_dict.py
@@ -10,3 +10,11 @@ print('d[\'Michael\'] =', d['Michael'])
 print('d[\'Bob\'] =', d['Bob'])
 print('d[\'Tracy\'] =', d['Tracy'])
 print('d.get(\'Thomas\', -1) =', d.get('Thomas', -1))
+
+# To add another name in d, example adding 'Gautam':25 to d
+
+d.update({'Gautam':25})
+
+
+print('d[\'Gautam\'] =', d['Gautam'])
+
