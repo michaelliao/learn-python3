06ea0ff9337d0285ae08bd11f403e5a3cc670ee8
admin <1067788906@qq.com>
2022-08-11 21:38:01 +0800

    test

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Python 3 教程
 ============
 
 [Python 3 教程](https://www.liaoxuefeng.com/wiki/1016959663602400)
+mytest
--- samples/advance/do_generator.py
+++ samples/advance/do_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+print('test')
 s = (x * x for x in range(5))
 print(s)
 for x in s:
