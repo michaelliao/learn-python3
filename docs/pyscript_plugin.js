4164c5cd8627b91b33c7a7b4500cb923d5fc56ec
Michael Liao <askxuefeng@gmail.com>
2023-04-26 21:54:44 +0800

    Update pyscript_plugin.js

--- docs/pyscript_plugin.js
+++ docs/pyscript_plugin.js
@@ -5,19 +5,31 @@ export default class LearnPython3Plugin {
     }
 
     afterPyScriptExec(opt) {
-	console.log(opt);
         let
             tag = opt.pyScriptTag,
             outputId = tag.getAttribute('output'),
             $btn = $('button[outputId=' + outputId + ']'),
             $i = $btn.find('i');
-		$i.removeClass('uk-icon-spinner');
-		$i.removeClass('uk-icon-spin');
+        $i.removeClass('uk-icon-spinner');
+        $i.removeClass('uk-icon-spin');
         $btn.removeAttr('disabled');
+        let err = $(tag).find('pre.py-error').text();
+        if (err) {
+            let
+                $out = $('#' + outputId),
+                s = $out.text();
+            if (s) {
+                s = s + '\n' + err;
+            } else {
+                s = err;
+            }
+            $out.text(s);
+            $out.addClass('uk-alert-danger');
+        }
     }
 
     onUserError(err) {
-	console.log('Error >>>');
+        console.log('Error >>>');
         console.error(err);
     }
 }
