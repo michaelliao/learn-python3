f983b17f6b8c6848c76158c892e3b77e47b5e7c8
Michael Liao <askxuefeng@gmail.com>
2023-04-26 20:23:20 +0800

    Update pyscript_plugin.js

--- docs/pyscript_plugin.js
+++ docs/pyscript_plugin.js
@@ -3,4 +3,12 @@ export default class LearnPython3Plugin {
         console.log('[plugin] pyscript startup.');
         window.__pyscript_ready__ = true;
     }
+
+    afterPyScriptExec(opt) {
+        let
+            tag = opt.pyScriptTag,
+            outputId = tag.getAttribute('output'),
+            $btn = $('button[outputId=' + outputId + ']);
+        $button.removeAttr('disabled');
+    }
 }
