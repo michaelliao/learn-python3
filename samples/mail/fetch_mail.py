05b9bace4f69625742c6d5980d64b054212dd038
SK <14342050+seabreeze2000@users.noreply.github.com>
2023-05-01 12:29:07 +0900

    Revert "comit1"
    This reverts commit 2e6cfd33d88d9e932f13dce88204ee2b70fa54a5.

--- samples/mail/fetch_mail.py
+++ samples/mail/fetch_mail.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# test1
 
 from email.parser import Parser
 from email.header import decode_header
--- samples/mail/send_mail.py
+++ samples/mail/send_mail.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# test1
 
 from email import encoders
 from email.header import Header
