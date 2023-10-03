27f49401960be8233d2c52b9e214ac9da7be22d3
Michael Liao <askxuefeng@gmail.com>
2023-10-03 13:05:24 +0800

    Create do_match.py

---
+++ samples/basic/do_match.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 简单匹配:
+print('-- simple match --')
+
+score = 'A'
+
+match score:
+    case 'A':
+        print('score is A.')
+    case 'B':
+        print('score is B.')
+    case 'C':
+        print('score is C.')
+    case _:
+        print('score is ???.')
+
+# 复杂匹配:
+print('-- complex match --')
+
+age = 15
+
+match age:
+    case x if x < 10:
+        print('< 10 years old.')
+    case 10:
+        print('10 years old.')
+    case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+        print('11~18 years old.')
+    case 19:
+        print('19 years old.')
+    case _:
+        print('not sure.')
+
+# 列表匹配:
+print('-- match list --')
+
+args = ['gcc', 'hello.c', 'world.c']
+# args = ['clean']
+# args = ['gcc']
+
+match args:
+    # 如果仅出现gcc，报错:
+    case ['gcc']:
+        print('gcc: missing source file(s).')
+    # 出现gcc，且至少指定了一个文件:
+    case ['gcc', file1, *files]:
+        print('gcc compile: ' + file1 + ', ' + ', '.join(files))
+    # 仅出现clean:
+    case ['clean']:
+        print('clean')
+    case _:
+        print('invalid command.')
